18d40fc4fcb81a77697d7e5f06fb5d3f43377f7f
Manfred Moser <manfred@simpligility.com>
2015-01-06 11:37:18 -0800

    removed unneeded import

--- src/test/java/com/jayway/maven/plugins/android/standalonemojos/LintMojoIntegrationTest.java
+++ src/test/java/com/jayway/maven/plugins/android/standalonemojos/LintMojoIntegrationTest.java
@@ -3,7 +3,6 @@ package com.jayway.maven.plugins.android.standalonemojos;
 import com.jayway.maven.plugins.android.*;
 import io.takari.maven.testing.TestResources;
 import io.takari.maven.testing.executor.MavenExecutionResult;
-import io.takari.maven.testing.executor.MavenInstallations;
 import io.takari.maven.testing.executor.MavenRuntime;
 import io.takari.maven.testing.executor.MavenVersions;
 import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
